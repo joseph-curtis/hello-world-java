6308c6516dc0303de563078557b8418f13aef0dc
Joseph Curtis <joe.curtis@pcc.edu>
2021-01-28 11:01:32 -0800

    print date

--- src/edu/pcc/cis233j/helloworld/HelloWorld.java
+++ src/edu/pcc/cis233j/helloworld/HelloWorld.java
@@ -1,5 +1,7 @@
 package edu.pcc.cis233j.helloworld;
 
+import java.util.Date;
+
 /**
  * @author Joseph Curtis
  * @version 2021.01.28
@@ -7,5 +9,6 @@ package edu.pcc.cis233j.helloworld;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println(new Date().toString());
     }
 }
