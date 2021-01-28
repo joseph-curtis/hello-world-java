1793630788dae7140fea3424afbaaf6b20da151a
Joseph Curtis <joe.curtis@pcc.edu>
2021-01-28 10:57:20 -0800

    added main method that prints hello world

--- src/edu/pcc/cis233j/helloworld/HelloWorld.java
+++ src/edu/pcc/cis233j/helloworld/HelloWorld.java
@@ -1,4 +1,11 @@
 package edu.pcc.cis233j.helloworld;
 
+/**
+ * @author Joseph Curtis
+ * @version 2021.01.28
+ */
 public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
 }
